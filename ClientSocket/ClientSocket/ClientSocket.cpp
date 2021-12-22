f3c4343e5fe15866b5d1c597efddc6f6c01be9e7
Th-release.cth <84012697+th-release@users.noreply.github.com>
2021-12-23 08:55:50 +0900

    Add files via upload

---
+++ ClientSocket/ClientSocket/ClientSocket.cpp
@@ -0,0 +1,35 @@
+﻿// ClientSocket.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <WinSock2.h>
+#pragma warning(suppress : 4996)
+#pragma comment (lib, "ws2_32")
+
+#define PORT 4578
+#define PACKET_SIZE 1024
+
+int main() {
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	SOCKET hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tAddr = {};
+	tAddr.sin_family = AF_INET;
+	tAddr.sin_port = htons(PORT);
+	tAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+	char cMsg[] = "클라이언트에서 알립니다.";
+	send(hSocket, cMsg, strlen(cMsg), 0);
+
+	char cBuffer[PACKET_SIZE] = {};
+	recv(hSocket, cBuffer, PACKET_SIZE, 0);
+
+	std::cout << "받은 메세지 : " << cBuffer;
+	closesocket(hSocket);
+
+	WSACleanup();
+	return 0;
+}
---
+++ Socket/Socket/Socket.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32")
+
+#define PORT 4578
+#define PACKET_SIZE 1024
+
+int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    // # 소켓을 사용하기위한 헤더 include 및 기본설정 지정 7 ~ 8 WSACleanup();
+    SOCKET hListen;
+    hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // # 소켓 생성 10 ~ 11
+    SOCKADDR_IN tListenAddr = { };
+    tListenAddr.sin_family = AF_INET;
+    tListenAddr.sin_port = htons(PORT); 
+    tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // # 소켓의 구성요소를 담을 구조체 생성 및 값 할당 13 ~ 16
+
+    bind(hListen, (SOCKADDR*)&tListenAddr, sizeof(tListenAddr));
+    listen(hListen, SOMAXCONN);
+    // # 소켓에 위에 설정한 주소정보를 묶어주고, 소켓을 접속대기상태로 만들어줌
+
+    SOCKADDR_IN tClntAddr = {};
+    int iClntSize = sizeof(tClntAddr);
+    SOCKET hClient = accept(hListen, (SOCKADDR*)&tClntAddr, &iClntSize);
+    // # 클라이언트 측 소켓 생성 및 정보를 담을 구조체 생성 및 값 할당, 클라이언트가 접속 요청하면 승인해주는 역할 22 ~ 24
+    
+    char cBuffer[PACKET_SIZE] = {};
+    recv(hClient, cBuffer, PACKET_SIZE, 0);
+    std::cout << "받은 메세지 : " << cBuffer;
+    char cMsg[] = "서버에서 알립니다.";
+    send(hClient, cMsg, strlen(cMsg), 0);
+
+    closesocket(hClient);
+    closesocket(hListen);
+    // #  클라이언트 측으로부터 정보를 받아오고 출력, 클라이언트에 정보 전송 28 ~ 35
+
+    WSACleanup();
+}
